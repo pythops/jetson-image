0134ba6209fef05a4a015fee9facffdea7386e99
Kapil Sachdeva <ksachdeva17@gmail.com>
2021-06-05 10:51:44 -0500

    use an environment variable to specify the board revision for 4GB nano

--- create-image.sh
+++ create-image.sh
@@ -10,26 +10,26 @@ BSP=https://developer.nvidia.com/embedded/l4t/r32_release_v5.1/r32_release_v5.1/
 
 # Check if the user is not root
 if [ "x$(whoami)" != "xroot" ]; then
-        printf "\e[31mThis script requires root privilege\e[0m\n"
-        exit 1
+    printf "\e[31mThis script requires root privilege\e[0m\n"
+    exit 1
 fi
 
 # Check for env variables
 if [ ! $JETSON_ROOTFS_DIR ] || [ ! $JETSON_BUILD_DIR ]; then
-	printf "\e[31mYou need to set the env variables \$JETSON_ROOTFS_DIR and \$JETSON_BUILD_DIR\e[0m\n"
-	exit 1
+    printf "\e[31mYou need to set the env variables \$JETSON_ROOTFS_DIR and \$JETSON_BUILD_DIR\e[0m\n"
+    exit 1
 fi
 
 # Check if $JETSON_ROOTFS_DIR if not empty
 if [ ! "$(ls -A $JETSON_ROOTFS_DIR)" ]; then
-	printf "\e[31mNo rootfs found in $JETSON_ROOTFS_DIR\e[0m\n"
-	exit 1
+    printf "\e[31mNo rootfs found in $JETSON_ROOTFS_DIR\e[0m\n"
+    exit 1
 fi
 
 # Check if board type is specified
 if [ ! $JETSON_NANO_BOARD ]; then
-	printf "\e[31mJetson nano board type must be specified\e[0m\n"
-	exit 1
+    printf "\e[31mJetson nano board type must be specified\e[0m\n"
+    exit 1
 fi
 
 printf "\e[32mBuild the image ...\n"
@@ -39,10 +39,10 @@ mkdir -p $JETSON_BUILD_DIR
 
 # Download L4T
 if [ ! "$(ls -A $JETSON_BUILD_DIR)" ]; then
-        printf "\e[32mDownload L4T...       "
-        wget -qO- $BSP | tar -jxpf - -C $JETSON_BUILD_DIR
-	rm $JETSON_BUILD_DIR/Linux_for_Tegra/rootfs/README.txt
-        printf "[OK]\n"
+    printf "\e[32mDownload L4T...       "
+    wget -qO- $BSP | tar -jxpf - -C $JETSON_BUILD_DIR
+    rm $JETSON_BUILD_DIR/Linux_for_Tegra/rootfs/README.txt
+    printf "[OK]\n"
 fi
 
 cp -rp $JETSON_ROOTFS_DIR/*  $JETSON_BUILD_DIR/Linux_for_Tegra/rootfs/ > /dev/null
@@ -63,21 +63,17 @@ case "$JETSON_NANO_BOARD" in
         printf "OK\n"
         ;;
 
-    jetson-nano-4gb-a02)
-        printf "Create image for Jetson nano 4GB (A02) board"
-        ./jetson-disk-image-creator.sh -o jetson.img -b jetson-nano -r 200
+    jetson-nano)
+        # default value for boad rev is 200
+        nano_board_rev="${JETSON_NANO_REVISION:=200}"
+        printf "Create image for Jetson nano 4GB ($nano_board_rev) board"
+        ./jetson-disk-image-creator.sh -o jetson.img -b jetson-nano -r $nano_board_rev
         printf "OK\n"
         ;;
-
-    jetson-nano-4gb-b01)
-        printf "Create image for Jetson nano 4GB (B01) board"
-        ./jetson-disk-image-creator.sh -o jetson.img -b jetson-nano -r 300
-        printf "OK\n"
-        ;;
-
+    
     *)
-	printf "\e[31mUnknown Jetson nano board type\e[0m\n"
-	exit 1
+    printf "\e[31mUnknown Jetson nano board type\e[0m\n"
+    exit 1
         ;;
 esac
 
