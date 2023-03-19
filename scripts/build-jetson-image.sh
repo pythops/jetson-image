1c0b0513306b4935a8ac21d3f1dc336fc8204459
pythops <contact@pythops.com>
2023-03-19 19:51:06 +0100

    update scripts/build-jetson-image.sh

--- scripts/build-jetson-image.sh
+++ scripts/build-jetson-image.sh
@@ -10,19 +10,26 @@ podman build \
 	-f Containerfile.image \
 	-t jetson-build-image
 
-# Temporary
 podman save -o build.tar localhost/jetson-build-image
 sudo podman load -i build.tar
 
-sudo podman run \
-	--user root \
-	--device-cgroup-rule="b 7:* rwm" \
-	--device /dev/loop-control:/dev/loop-control:rwm \
-	--privileged \
-	--cap-add=all \
-	--rm \
-	-it \
-	-v .:/jetson \
-	-e JETSON_NANO_BOARD="jetson-nano" \
-	localhost/jetson-build-image:latest \
-	create-jetson-image.sh
+if [ "$1" == "jetson-nano" ]; then
+	sudo podman run \
+		--rm \
+		--privileged \
+		-v .:/jetson \
+		-e JETSON_NANO_BOARD="jetson-nano" \
+		-e JETSON_NANO_REVISION="$2" \
+		localhost/jetson-build-image:latest \
+		create-jetson-image.sh
+else
+	sudo podman run \
+		--rm \
+		--privileged \
+		-v .:/jetson \
+		-e JETSON_NANO_BOARD="jetson-nano-2gb" \
+		localhost/jetson-build-image:latest \
+		create-jetson-image.sh
+fi
+
+sudo rm -rf build.tar
