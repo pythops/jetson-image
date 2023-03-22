4e6f9a396256876fd7d4eecc1290f2063805eb08
pythops <contact@pythops.com>
2023-03-22 20:02:51 +0100

    resize the rootfs at the boot time

---
+++ scripts/resizerootfs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+rootpart="/dev/$(lsblk -l -o NAME,MOUNTPOINT | grep '/' | awk '{print $1}')"
+rootdevice="/dev/$(lsblk -no pkname $rootpart)"
+
+partprobe "$rootdevice"
+
+echo ", +" | sfdisk -f -N 1 "$rootdevice"
+
+resize2fs "$rootpart"
+
+partprobe "$rootdevice"
