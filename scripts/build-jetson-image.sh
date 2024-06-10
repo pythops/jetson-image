aefab06a542906a204f845f06eb965d2a89ed5f3
pythops <contact@pythops.com>
2024-06-11 00:23:09 +0200

    Possibility to include Nvidia packages

--- scripts/build-jetson-image.sh
+++ scripts/build-jetson-image.sh
@@ -129,9 +129,23 @@ case $board in
 *) ;;
 esac
 
+L4T_PACKAGES=""
+
+if [[ -f "l4t_packages.txt" ]]; then
+  while IFS= read -r line; do
+    if [[ ${line} != \#* ]]; then
+      L4T_PACKAGES+=" $line"
+    fi
+  done <"l4t_packages.txt"
+
+fi
+
+L4T_PACKAGES="${L4T_PACKAGES# }"
+
 sudo -E XDG_RUNTIME_DIR= DBUS_SESSION_BUS_ADDRESS= podman build \
   --cap-add=all \
   --jobs=4 \
+  --build-arg L4T_PACKAGES="$L4T_PACKAGES" \
   -f Containerfile.image.l4t"$l4t" \
   -t jetson-build-image-l4t"$l4t"
 
